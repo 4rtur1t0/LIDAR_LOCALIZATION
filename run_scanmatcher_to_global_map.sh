cd4a91d21c1d385d3a16760c70a58ce2466b052a
Arturo <arturo.gil@umh.es>
2025-07-18 17:23:33 +0200

    Cleaning

--- run_scanmatcher_to_global_map.sh
+++ run_scanmatcher_to_global_map.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-source ~/Applications/venv2/bin/activate
-export PYTHONPATH=~/Applications/venv2/lib/python3.8/site-packages:/opt/ros/noetic/lib/python3/dist-packages:/usr/lib/python3/dist-packages:
+PYTHON_VENV=~/Applications/venv2
+source $PYTHON_VENV/bin/activate
+export PYTHONPATH=$PYTHON_VENV/lib/python3.8/site-packages:/opt/ros/noetic/lib/python3/dist-packages:/usr/lib/python3/dist-packages:
 python run_scanmatcher_to_global_map.py
